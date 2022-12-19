a23931637d59e97907bfc92f280647706612645e
Thiago Diniz <thiagodiniz.sjc@gmail.com>
2022-12-19 18:37:53 -0300

    criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import Title from "../Title/title"
 import Subtitle from "../subtitle/Subtitle"
 import styles from './Header.module.css'
+import Icon from "../icon/Icon"
 
 function Header(){
   return(
     <div className={styles.header}>
       <Title>Jogo da velha</Title>
       <Subtitle>Criado Subtitleor: Thiago Diniz</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/ThiagoDinizz"/>
+      </div>
     </div>
   )
 
