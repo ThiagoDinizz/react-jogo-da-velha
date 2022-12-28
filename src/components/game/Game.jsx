f348d7b9c5a031e2711da894d971f3524725cd16
Thiago Diniz <thiagodiniz.sjc@gmail.com>
2022-12-28 16:52:37 -0300

    criado lógica a lógica do vencedor

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,21 +1,47 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOpition'
 import Icon from '../icon/Icon'
 
+const winnerTable = [
+ [0, 1, 2],
+ [3, 4, 5],
+ [6, 7, 8],
+ [0, 3, 6],
+ [1, 4, 7],
+ [2, 5, 8],
+ [0, 4, 8],
+ [2, 4, 6]
+
+]
+
 function Game(){
   const [gameState,setGameState] = useState(Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(-1)
+  const [winner, setWinner] = useState(0)
   
   const handleClick = (pos) =>{
-    if (gameState[pos]===0){
+    if (gameState[pos]===0 && winner === 0){
       let newGameState = [...gameState]
       newGameState[pos] = currentPlayer
-      setCurrentPlayer(currentPlayer *-1)
       setGameState(newGameState)
     }
   }
+
+  const verifyGame = () => {
+      winnerTable.forEach((line)=>{
+      const values = line.map((pos)=> gameState[pos])
+      const sum = values.reduce((sum, value)=>sum + value)
+      if(sum === 3 || sum === -3) setWinner(sum / 3)
+    })
+
+  }
+
+  useEffect( () => { 
+    setCurrentPlayer(currentPlayer *-1)
+    verifyGame()
+  }, [gameState])
 
   return(
     <div className={styles.gameContent}>
@@ -29,8 +55,8 @@ function Game(){
         />
         )
       }
-      </div>
-      <div className={styles.gameInfo}>
+        </div>
+        <div className={styles.gameInfo}>
         <h4>Próximo a jogar:</h4>
         {
           currentPlayer ===1 && <Icon iconName="circle" />
@@ -39,7 +65,7 @@ function Game(){
           currentPlayer ===-1 && <Icon iconName="x" />
         }
 
-      </div>
+        </div>
 
       </div>
 
